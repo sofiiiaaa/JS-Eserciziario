8213bf8b88b5cf1f74f54a2dc93d33be391814e0
Sofia Riggi <sofiariggi@libero.it>
2022-02-14 17:42:22 +0100

    new_quotations

--- exercises/generate_quotations/index.js
+++ exercises/generate_quotations/index.js
@@ -1,17 +1,31 @@
 const button = document.getElementById("btn");
 const quotation = document.getElementById("api-quotation");
-let url = "https://www.quotepub.com/api/widget/?type=rand";
+let url = "https://type.fit/api/quotes";
 
 
 
-function getData () {
-  axios.get(url)
-  .then((raw) => raw.data[0].quote_body)         
-  .then ((newData)=> {quotation.innerHTML = "here's your quotation: <br> <br>" + newData})                               
-  .then(() => quotation.style.border = "green solid")
-  .catch ((err) =>alert (err))                
-  }
+// function getData () {
+//   axios.get(url)
+//   .then((raw) => raw.data[0].quote_body)         
+//   .then ((newData)=> {quotation.innerHTML = "here's your quotation: <br> <br>" + newData})                               
+//   .then(() => quotation.style.border = "green solid")
+//   .catch ((err) =>alert (err))                
+//   }
             
-button.addEventListener ("click", getData) 
+// button.addEventListener ("click", getData) 
  
+function getData(){
+  console.log('here')
+  axios.get(url)  
+  .then ((raw)=> raw.data)
+  .then ((data)=> {
+       let newQuotation = Math.floor((Math.random()*1000)+600);
+       return data[newQuotation]       
+  })
+  .then ((result)=> quotation.innerHTML= "here's your quotation of the day: <br>" + result.text + "<br> <br> cit- by: <br>" + result.author)
+  .then (()=> quotation.style.border= "green solid")
+  .catch((err)=> alert(err))
+}
+
+button.addEventListener("click", getData)
 
